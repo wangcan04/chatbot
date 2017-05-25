4b3ff5df068d6cf0d0fc9cfd26beb31a38776f6e
Can WANG <wangcan0429@gmail.com>
2017-05-25 13:57:25 +0800

    Update word2vec.py

--- word2vec.py
+++ word2vec.py
@@ -2,7 +2,7 @@ import gensim
 import multiprocessing
 from singa import tensor
 import numpy as np
-
+from singa import device
 FILENAME = 'dataclean.txt'
 
 
@@ -20,7 +20,7 @@ def word2vec():
 
 def question2vec(i,max):
     model = gensim.models.Word2Vec.load("word2vec_gensim")
-    lines = open("dataclean.txt").read().split('\n')
+    lines = open("dataq.txt").read().split('\n')
     se = lines[i].split()
     vec = []
     if '' in se:
@@ -35,9 +35,9 @@ def question2vec(i,max):
         vec = pad
     return vec
 
-def answer2vec(i):
+def answer2vec(i,max):
     model = gensim.models.Word2Vec.load("word2vec_gensim")
-    lines = open("dataclean.txt").read().split('\n')
+    lines = open("dataa.txt").read().split('\n')
     se = lines[i].split()
     vec = []
     if '' in se:
@@ -45,11 +45,15 @@ def answer2vec(i):
     for x in se:
         vec.append(model.wv[x])
     vec = np.array(vec)
+    if len(vec) < max:
+        pad = np.zeros((max, 100), dtype=np.float32)
+        for i in range(len(vec)):
+            pad[i] = vec[i]
+        vec = pad
     return vec
 
 def numpy2tensors(numpy,dev):
-    '''batch, seq, dim -- > seq, batch, dim'''
-    numpy = np.swapaxes(numpy, 0, 1)
+
     inputs = []
     for t in range(numpy.shape[0]):
         num = tensor.from_numpy(numpy[t])
@@ -57,14 +61,5 @@ def numpy2tensors(numpy,dev):
         inputs.append(num)
     return inputs
 
-def convert(batch,batch_size,seq_length,vocab_size, dev):
-    x=np.zeros((batch_size, seq_length, vocab_size), dtype=np.float32)
-    for b in range(batch_size):
-        for t in range(seq_length):
-            c=batch[b,t]
-            x[b,t,c]=1
-    return numpy2tensors(x,dev)
-
 if __name__ == '__main__':
     word2vec()
-

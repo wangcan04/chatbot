81a9c8cb34efd0f75e1ef72c92362e9c42f0b908
Can WANG <can.wang@student.ecp.fr>
2017-06-22 16:02:34 +0800

    Create predict.py

---
+++ ChatbotIndex/predict.py
@@ -0,0 +1,41 @@
+import cPickle as pickle
+import numpy as np
+import argparse
+import pprint
+
+# sys.path.append(os.path.join(os.path.dirname(__file__), '../../build/python'))
+from singa import layer
+from singa import loss
+from singa import device
+from singa import tensor
+from singa import optimizer
+from singa import initializer
+from singa.proto import model_pb2
+from tqdm import tnrange
+from word2tensor import load_data,numpy2tensors,convert,labelconvert
+import preprocess
+import time
+
+if __name__ == "__main__":
+        model_file = open('71.bin', 'rb')
+        param = pickle.load(model_file)
+        model_file.close()
+        decoderw=param['decoder_w']
+        densew,denseb=param['dense_w'],param['dense_b']
+        hiddensize=param['hidden_size']
+        numstacks=param['num_stacks']
+        drop_out=param['dropout']
+        vocab_size=7000
+        cuda = device.create_cuda_gpu_on(1)
+        encoder = layer.LSTM(name='lstm1', hidden_size=hiddensize, num_stacks=numstacks, dropout=drop_out, input_sample_shape=(vocab_size,))
+        decoder = layer.LSTM(name='lstm2', hidden_size=hiddensize, num_stacks=numstacks, dropout=drop_out, input_sample_shape=(vocab_size,))
+        encoder.to_device(cuda)
+        decoder.to_device(cuda)
+        encoder_w = encoder.param_values()[0]
+        encoder_w.uniform(-0.08, 0.08)
+        decoder.param_values()[0].copy_from_numpy(decoderw, offset=0)
+
+        dense = layer.Dense('dense', vocab_size, input_sample_shape=(hiddensize,))
+        dense.to_device(cuda)
+        dense.param_values()[0].copy_from_numpy(densew,offset=0)
+        dense.param_values()[1].copy_from_numpy(denseb,offset=0)

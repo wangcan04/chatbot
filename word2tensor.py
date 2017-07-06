809b4706c341a4bcca8a695234031edacb777029
Can WANG <can.wang@student.ecp.fr>
2017-07-06 10:19:38 +0800

    Update and rename word2vec.py to word2tensor.py

--- word2tensor.py
+++ word2tensor.py
@@ -1,22 +1,19 @@
+import numpy as np
+from singa import tensor
+import pickle
 import gensim
 import multiprocessing
-from singa import tensor
-import numpy as np
 from singa import device
-FILENAME = 'dataclean.txt'
 
+def load_data(PATH=''):
+    # read data control dictionaries
+    with open(PATH + 'metadata.pkl', 'rb') as f:
+        metadata = pickle.load(f)
+    # read numpy arrays
+    idx_q = np.load(PATH + 'idx_q.npy')
+    idx_a = np.load(PATH + 'idx_a.npy')
+    return metadata, idx_q, idx_a
 
-def word2vec():
-    sentences = gensim.models.word2vec.LineSentence(FILENAME)
-    model = gensim.models.Word2Vec(sentences,
-                                  size=100,
-                                window=30,
-                                min_count=1,
-                                workers=multiprocessing.cpu_count())
-    model.save("word2vec_gensim")
-    model.wv.save_word2vec_format("word2vec_org",
-                              "vocabulary",
-                              binary=False)
 
 def question2vec(i,max):
     model = gensim.models.Word2Vec.load("word2vec_gensim")
@@ -35,6 +32,7 @@ def question2vec(i,max):
         vec = pad
     return vec
 
+
 def answer2vec(i,max):
     model = gensim.models.Word2Vec.load("word2vec_gensim")
     lines = open("dataa.txt").read().split('\n')
@@ -52,14 +50,29 @@ def answer2vec(i,max):
         vec = pad
     return vec
 
-def numpy2tensors(numpy,dev):
-    x = np.swapaxes(numpy, 0, 1)
+
+def numpy2tensors(num, dev):
+    '''batch, seq, dim -- > seq, batch, dim'''
+    tmpx = np.swapaxes(num, 0, 1)
     inputs = []
-    for t in range(x.shape[0]):
-        num = tensor.from_numpy(x[t])
-        num.to_device(dev)
-        inputs.append(num)
+    for t in range(tmpx.shape[0]):
+        x = tensor.from_numpy(tmpx[t])
+        x.to_device(dev)
+        inputs.append(x)
     return inputs
 
-if __name__ == '__main__':
-    word2vec()
+
+
+def labelconvert(batch,dev):
+    return numpy2tensors(batch,dev)
+
+'''
+def convert(batch, batch_size, seq_length, vocab_size, dev):
+    x = np.zeros((batch_size, seq_length, vocab_size), dtype=np.float32)
+    for b in range(batch_size):
+        for t in range(seq_length):
+            c = batch[b, t]
+            x[b, t, c] = 1
+    return numpy2tensors(x,dev)
+'''
+       

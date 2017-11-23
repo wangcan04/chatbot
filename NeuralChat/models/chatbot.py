476780133a4b90ff55253d20b5dcd53d4efbf11e
Can WANG <can.wang@student.ecp.fr>
2017-11-23 13:29:00 +0800

    Update chatbot.py

--- NeuralChat/models/chatbot.py
+++ NeuralChat/models/chatbot.py
@@ -96,19 +96,23 @@ class ChatbotModel(object):
                       output_attention=False,
                       name='attention_wrapper'
                       )#attention layer
-            batch_size=tf.shape(encoder_outputs)[0]
-            attn_zero=attn_cell.zero_state(batch_size=batch_size,dtype=tf.float32)
-            init_state=attn_zero.clone(cell_state=encoder_state)
         if decoder_mode:
+            beam_width=2
+            batch_size=tf.shape(encoder_outputs)[0]
+            attn_zero=attn_cell.zero_state(batch_size=batch_size*beam_width,tf.float32)
+            init_state=attn_zero.clone(cell_state=encoder_state)
             decoder = seq2seq.BeamSearchDecoder(cell=decoder_cell,embedding=embeddings,
                                                 start_tokens=tf.tile([GO_ID], [1]),
                                                 end_token=EOS_ID,
                                                 initial_state=encoder_state[-1],
-                                                beam_width=1)#BeamSearch in Decoder
+                                                beam_width=beam_width)#BeamSearch in Decoder
             final_outputs, final_state, final_sequence_lengths =\
                             seq2seq.dynamic_decode(decoder=decoder)
             self.logits = final_outputs.predicted_ids
         else:
+            batch_size=tf.shape(encoder_outputs)[0]
+            attn_zero=attn_cell.zero_state(batch_size=batch_size,dtype=tf.float32)
+            init_state=attn_zero.clone(cell_state=encoder_state)
             helper = seq2seq.TrainingHelper(inputs=targets_embedding,
                                             sequence_length=self.target_lengths)
 
